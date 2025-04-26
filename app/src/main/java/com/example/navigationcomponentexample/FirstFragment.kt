760e9c0bf72a11149d7d72e440233822bd04ce8b
csantyt <carlos.torres.c@uniautonoma.edu.co>
2025-04-25 20:20:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponentexample/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.navigationcomponentexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root: View = inflater.inflate(R.layout.fragment_first, container, false)
+        val btnNavigate : Button? = root.findViewById<Button>(R.id.btnNavigate)
+
+        btnNavigate?.setOnClickListener {
+            findNavController().navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment(
+                Name  ="AristiDevs") )
+        }
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.example.navigationcomponentexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.navArgs
+
+class SecondFragment : Fragment() {
+    val args: SecondFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val Name = args.Name
+        val tvName : TextView = view.findViewById<TextView>(R.id.tvName)
+        tvName.text = Name
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+}
